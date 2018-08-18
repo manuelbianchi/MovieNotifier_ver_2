759c43166675467ea2ccc2853a9d2a45d422f48f
Manuel <manuel.bianchi1@gmail.com>
2018-08-18 23:41:09 +0200

    Done: - realizzato alert con i radiobutton ed una textView - realizzato i bottoni nella movieActivity ToDo: - sistema l'alert mettendo un calendario e mettendo a posto nomi e sistemando il layout. - per vedere i bottoni è necessario scrollare. - iniziare a salvare le informazioni pop-up e riempire la tab notify con le informazioni dei film (title e release). - realizzare un sistema di notifiche push e alarm (suono di notifica).

--- app/src/main/java/com/example/msnma/movienotifier/MovieActivity.java
+++ app/src/main/java/com/example/msnma/movienotifier/MovieActivity.java
@@ -2,8 +2,10 @@ package com.example.msnma.movienotifier;
 
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,12 +21,24 @@ import android.widget.RadioGroup;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.iconics.IconicsDrawable;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MovieActivity extends BaseActivity {
 
     private MovieFragment movieFrag;
+
+    //parte per il popup data
+    Context context = this;
+    EditText editDate;
+    Calendar myCalendar = Calendar.getInstance();
+    String dateFormat = "dd.MM.yyyy";
+    DatePickerDialog.OnDateSetListener date;
+    SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.GERMAN);
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -45,7 +60,7 @@ public class MovieActivity extends BaseActivity {
         menu.findItem(R.id.share)
                 .setIcon(new IconicsDrawable(this)
                         .icon(GoogleMaterial.Icon.gmd_share)
-                        .color(Color.BLACK)
+                        .color(Color.WHITE)
                         .sizeDp(24));
         return true;
     }
@@ -97,6 +112,45 @@ public class MovieActivity extends BaseActivity {
         final EditText nameEditText = (EditText) formElementsView
                 .findViewById(R.id.nameEditText);
 
+        //parte data
+/*
+        editDate = (EditText) findViewById(R.id.nameEditText);
+
+// init - set date to current date
+        long currentdate = System.currentTimeMillis();
+        String dateString = sdf.format(currentdate);
+        editDate.setText(dateString);
+
+// set calendar date and update editDate
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDate();
+            }
+
+        };
+
+// onclick - popup datepicker
+        editDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(context, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });*/
+
+
+
+
         // the alert dialog
         new AlertDialog.Builder(MovieActivity.this).setView(formElementsView)
                 .setTitle("Form Elements")
@@ -132,8 +186,8 @@ public class MovieActivity extends BaseActivity {
                         /*
                          * Getting the value of an EditText.
                          */
-                        toastString += "Name is: " + nameEditText.getText()
-                                + "!\n";
+                        /*toastString += "Name is: " + nameEditText.getText()
+                                + "!\n";*/
 
                         //showToast(toastString);
 
@@ -142,5 +196,24 @@ public class MovieActivity extends BaseActivity {
 
                 }).show();
     }
+
+
+    private void updateDate() {
+        editDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
+
+
+
+     public void pressEditNotifyButton(View view)
+     {
+         alertFormElements();
+     }
+            //Intent myIntent = new Intent(view.getContext(), agones.class);
+            //startActivityForResult(myIntent, 0);
+
+
+
+
 
 }
